f5fc6fbdd965173f7cf73f722b974734d4218bfc
Vanessa Carraro <81984134+B1olog4@users.noreply.github.com>
2022-04-26 18:01:30 -0300

    Add files via upload

---
+++ CLONEFLIX/CLONEFLIX/js/main.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('[data-js="carosel_item"]');
+const nextButton = document.querySelector('[data-js="carosel_button--next"]');
+
+let currentSlideIndex = 0;
+
+nextButton.addEventListener('click', () => {
+    currentSlideIndex++;
+
+    slides.forEach(slide => {
+        slide.classList.remove('carousel__item--visible')
+    })
+});
+slides[currentSlideIndex].classList.add('carousel__item--visible');
+
+
+
